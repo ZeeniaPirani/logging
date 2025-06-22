cecf9ac7bf84dd7c2405e503525b9b50aaa2712c
ZeeniaPirani <zeenia0315@gmail.com>
2025-06-22 07:31:17 -0700

    comments

--- app_recorder.py
+++ app_recorder.py
@@ -2,27 +2,35 @@ import psutil
 from datetime import datetime
 import time
 
+# path to logging file, can change depending on which file logging should be done in
 log_file = "running_apps_log.txt"
 
+# try-except allows user to exit logger without throwing error
 try:
     with open(log_file, 'a') as f:
         timestamp = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
         while True:
+            # writes to logging file, records timestamp
             f.write(f"\n[{timestamp}] Currently Running: \n")
+            # creates set to make sure no duplicates are printed out
             seen = set()
 
+            # iterates through process names, adds to file and set if name not seen
             for proc in psutil.process_iter(["name"]):
                 try:
                     name = proc.info['name']
                     if name not in seen:
                         f.write(name + ", ")
                         seen.add(name)
+                # if permission to view process info is denied or process doesn't exist, skip name
                 except (psutil.NoSuchProcess, psutil.AccessDenied):
                     continue
     
+            # logs every 2 minutes, can change based on frequency of logging
             time.sleep(120)
 
 except KeyboardInterrupt:
     print("\nLogging stopped by user")
+    # writes timestamp of user stopping logging for accurate log files
     with open(log_file, 'a') as f:
         f.write("Logging stopped at " + datetime.now().strftime("%d/%m/%Y, %H:%M:%S") + "\n")
--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,16 @@
 from pynput import keyboard
 
+# path to logging file, can be changed depending on which file keystrokes should be stored in
 path = "data.txt"
 
 while True:
     with open(path, 'a') as data_file:
-
+        # continues logging keys until "`" is pressed
         events = keyboard.record('`')
+
+        # can extract typed strings from sequence of keyboard events, easy readability
         typed = list(keyboard.get_typed_strings(events))
 
+        # writes keystrokes into logging file
         data_file.write('\n')
         data_file.write(typed[0])
